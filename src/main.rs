4150fcf9a82816de464d6e75c5f5494867ec8656
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-10-03 00:03:12 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,71 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
+use std::io;
+use std::io::prelude::*;
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+#[derive(Parser, Debug)]
+#[command(version, author, propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    Init,
+    CatFile {
+        #[arg(short, long)]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
+}
+
+// Uncompresses a Zlib Encoded vector of bytes and returns a string or error
+// Here &[u8] implements Read
+
+fn decode_reader(bytes: Vec<u8>) -> io::Result<String> {
+    let mut z = ZlibDecoder::new(&bytes[..]);
+    let mut s = String::new();
+    z.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
+    use Command::*;
+    match cli.command {
+        Init => {
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+            println!("Initialized git directory")
+        }
+        CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            // let file = fs::OpenOptions::new()
+            //     .read(true)
+            //     .write(true)
+            //     .create(true)
+            //     .open("foo.txt");
+
+            let mut f = fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))?;
+
+            let mut z = ZlibDecoder::new(f);
+            let mut buf = String::new();
+            z.read_to_string(&mut buf)?;
+        }
+    }
+
+    Ok(())
 }
